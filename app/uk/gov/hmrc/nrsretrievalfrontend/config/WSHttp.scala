f2858b3d5779f0b67533cf619f6991b901768336
Chris Rudd <chris.rudd@digital.hmrc.gov.uk>
2018-03-01 13:54:25 +0000

    Added call to nackend endpoint

--- app/uk/gov/hmrc/nrsretrievalfrontend/config/WSHttp.scala
+++ app/uk/gov/hmrc/nrsretrievalfrontend/config/WSHttp.scala
@@ -18,14 +18,23 @@ package uk.gov.hmrc.nrsretrievalfrontend.config
 
 import javax.inject.{Inject, Singleton}
 
-import play.api.i18n.MessagesApi
-import play.api.mvc.Request
-import play.twirl.api.Html
-import uk.gov.hmrc.play.bootstrap.http.FrontendErrorHandler
-import uk.gov.hmrc.nrsretrievalfrontend.views
+import com.google.inject.ImplementedBy
+import play.api.{Configuration, Environment}
+import play.api.Mode.Mode
+import uk.gov.hmrc.http._
+import uk.gov.hmrc.play.config.{AppName, RunMode}
+import uk.gov.hmrc.play.http.ws._
+
+@ImplementedBy(classOf[WSHttp])
+trait WSHttpT extends HttpGet with WSGet
+  with HttpPut with WSPut
+  with HttpPost with WSPost
+  with HttpDelete with WSDelete
+  with HttpPatch with WSPatch
+  with AppName with RunMode
 
 @Singleton
-class ErrorHandler @Inject()(val messagesApi: MessagesApi, implicit val appConfig: AppConfig) extends FrontendErrorHandler {
-  override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: Request[_]): Html =
-    views.html.error_template(pageTitle, heading, message)
+class WSHttp @Inject() (val environment: Environment, val runModeConfiguration: Configuration, val appNameConfiguration: Configuration) extends WSHttpT {
+  val mode: Mode = environment.mode
+  override val hooks = NoneRequired
 }

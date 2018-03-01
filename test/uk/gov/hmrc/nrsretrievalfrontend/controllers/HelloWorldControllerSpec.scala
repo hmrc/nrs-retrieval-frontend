9a9088d4582f15accc32f34cad6e7fc17a7360df
Chris Rudd <chris.rudd@digital.hmrc.gov.uk>
2018-03-01 15:42:07 +0000

    Added scoverage config and mockito.

--- test/uk/gov/hmrc/nrsretrievalfrontend/controllers/HelloWorldControllerSpec.scala
+++ test/uk/gov/hmrc/nrsretrievalfrontend/controllers/HelloWorldControllerSpec.scala
@@ -1,15 +1,37 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package uk.gov.hmrc.nrsretrievalfrontend.controllers
 
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import org.scalatest.mockito.MockitoSugar
 import play.api.http.Status
 import play.api.i18n.{DefaultLangs, DefaultMessagesApi}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import play.api.{Configuration, Environment}
+import uk.gov.hmrc.http.HttpResponse
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
 import uk.gov.hmrc.nrsretrievalfrontend.config.AppConfig
+import uk.gov.hmrc.nrsretrievalfrontend.connectors.NrsRetrievalConnector
 
-class HelloWorldControllerSpec extends UnitSpec with WithFakeApplication {
+import scala.concurrent.Future
+
+class HelloWorldControllerSpec extends UnitSpec with WithFakeApplication with MockitoSugar {
   val fakeRequest = FakeRequest("GET", "/")
 
   val env = Environment.simple()
@@ -18,7 +40,13 @@ class HelloWorldControllerSpec extends UnitSpec with WithFakeApplication {
   val messageApi = new DefaultMessagesApi(env, configuration, new DefaultLangs(configuration))
   val appConfig = new AppConfig(configuration, env)
 
-  val controller = new HelloWorld(messageApi, appConfig)
+  val mockHttpRespone = mock[HttpResponse]
+  when(mockHttpRespone.body).thenReturn("Some Text")
+
+  val mockConnector = mock[NrsRetrievalConnector]
+  when(mockConnector.getHelloWorld()(any())).thenReturn(Future.successful(mockHttpRespone))
+
+  val controller = new HelloWorld(messageApi, mockConnector, appConfig)
 
   "GET /" should {
     "return 200" in {

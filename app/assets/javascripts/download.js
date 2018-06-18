3331e3e0f481ce03906dc49255063ae21f58563a
Alistair Macdonald <ali@nubz.com>
2018-06-18 11:53:01 +0100

    NONPR-648: created single result template

--- app/assets/javascripts/download.js
+++ app/assets/javascripts/download.js
@@ -1,41 +1,52 @@
-NrsAjax = {
-    http: function(index, vaultName, archiveId) {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onload = function (e) {
-            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-                setStatus(index, this.response)
-            } else {
-                NrsAjax.checkStatus(index, vaultName, archiveId);
-            }
-        };
-        xmlhttp.ontimeout = function() {
-            NrsAjax.checkStatus(index, vaultName, archiveId);
-        };
-        return xmlhttp;
+(function ($, window) {
+
+  var NrsAjax = {
+    http: function (index, vaultName, archiveId) {
+      console.log('NrsAjax.http(' + index + ', ' + vaultName + ', ' + archiveId + ')')
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onload = function (e) {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+          setStatus(index, this.response)
+        } else {
+          NrsAjax.checkStatus(index, vaultName, archiveId);
+        }
+      };
+      xmlhttp.ontimeout = function () {
+        NrsAjax.checkStatus(index, vaultName, archiveId);
+      };
+      return xmlhttp;
     },
-    checkStatus: function(index, vaultName, archiveId) {
-        var xmlhttp = this.http(index, vaultName, archiveId);
-        xmlhttp.open("GET", 'status/'+vaultName+'/'+archiveId);
-        xmlhttp.timeout = 30000;
-        xmlhttp.send();
+    checkStatus: function (index, vaultName, archiveId) {
+      var xmlhttp = this.http(index, vaultName, archiveId);
+      xmlhttp.open("GET", 'status/' + vaultName + '/' + archiveId);
+      xmlhttp.timeout = 30000;
+      xmlhttp.send();
     }
-};
+  };
 
-function setStatus(index, status) {
-    var success = document.getElementById("search-result-success-" + index);
-    var failed = document.getElementById("search-result-failed-" + index);
-    var inPorgress = document.getElementById("search-result-in-progress-" + index);
-
-    switch(status) {
-        case 'Complete':
-            success.style.display = "block";
-            failed.style.display = "none";
-            inPorgress.style.display = "none";
-            break;
-        case 'Failed':
-            success.style.display = "none";
-            failed.style.display = "block";
-            inPorgress.style.display = "none";
-            break;
+  function setStatus(index, status) {
+    var $target = $('#retrieve' + index).find('.result-retrieve')
+    switch (status) {
+      case 'Complete':
+        $target.addClass('retrieval-complete')
+        break;
+      case 'Failed':
+        $target.addClass('retrieval-failed')
+        break;
+      default:
+        $target.addClass('retrieval-in-progress')
     }
-}
+  }
+
+  // attach listeners to retrieval events
+  $('a.start-retrieval').on('click', function (e) {
+    e.preventDefault();
+    var $link = $(e.currentTarget)
+    var vaultName = $link.attr("data-vault-id")
+    var index = $link.attr("data-index")
+    var archiveId = $link.attr("data-archive-id")
+    setStatus(index, 'retrieval-in-progress')
+    NrsAjax.checkStatus(index,  vaultName, archiveId)
+  })
+
+})(jQuery, window);

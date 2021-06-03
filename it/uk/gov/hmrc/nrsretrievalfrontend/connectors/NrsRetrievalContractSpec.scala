a014d640b350515c2706e0647d02dc49c1086e15
PaulHodgson <paul.hodgson073@gmail.com>
2021-06-03 12:08:21 +0100

    complete connector contract test

--- it/uk/gov/hmrc/nrsretrievalfrontend/connectors/NrsRetrievalContractSpec.scala
+++ it/uk/gov/hmrc/nrsretrievalfrontend/connectors/NrsRetrievalContractSpec.scala
@@ -18,15 +18,20 @@ package uk.gov.hmrc.nrsretrievalfrontend.connectors
 
 import com.github.tomakehurst.wiremock.client.WireMock
 import connectors.NrsRetrievalConnector
+import models._
+import org.joda.time.LocalDate
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
-import org.scalatest.{BeforeAndAfterEach, Matchers, WordSpecLike}
+import org.scalatest.{Assertion, BeforeAndAfterEach, Matchers, WordSpecLike}
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.ws.WSResponse
 import play.api.test.Helpers.baseApplicationBuilder.injector
 import uk.gov.hmrc.http.{HeaderCarrier, HttpException, HttpResponse, UpstreamErrorResponse}
 import uk.gov.hmrc.nrsretrievalfrontend.stubs.NrsRetrievalStubs._
 import uk.gov.hmrc.nrsretrievalfrontend.wiremock.WireMockSupport
+
+import java.time.ZonedDateTime
 
 class NrsRetrievalContractSpec
   extends WordSpecLike
@@ -40,6 +45,8 @@ class NrsRetrievalContractSpec
 
   private lazy val connector = injector.instanceOf[NrsRetrievalConnector]
 
+  private val authorisedUser = AuthorisedUser("userName", "authProviderId")
+
   override def beforeEach(): Unit = WireMock.reset()
 
   override def fakeApplication(): Application =
@@ -49,9 +56,138 @@ class NrsRetrievalContractSpec
       "metrics.jvm" -> false)
     ).build()
 
+  private def anUpstreamErrorResponseShouldBeThrownBy[T](request: () => T, statusCode: Int): Assertion =
+    intercept[Exception] {
+      request()
+    }.getCause.asInstanceOf[UpstreamErrorResponse].statusCode shouldBe statusCode
+
+  private def anInternalServerErrorShouldBeThrownBy[T](request: () => T): Assertion =
+    anUpstreamErrorResponseShouldBeThrownBy(request, INTERNAL_SERVER_ERROR)
+
+  private def aBadGatewayErrorShouldBeThrownBy[T](request: () => T): Assertion =
+    anUpstreamErrorResponseShouldBeThrownBy(request, BAD_GATEWAY)
+
+  private def aNotFoundErrorShouldBeThrownBy[T](request: () => T): Assertion =
+    intercept[Exception] {
+      request()
+    }.getCause.asInstanceOf[HttpException].responseCode shouldBe NOT_FOUND
+
+  "search" should {
+    val searchQuery = SearchQuery(Some("searchKeyName_0"), Some("searchKeyValue_0"), "notableEventType")
+
+    def search(): Seq[NrsSearchResult] = connector.search(searchQuery, authorisedUser).futureValue
+
+    "return a sequence of results" when {
+      val results =
+        Seq(
+          NrsSearchResult(
+            "businessId",
+            "notableEvent",
+            "payloadContentType",
+            ZonedDateTime.now(),
+            None,
+            "userAuthToken",
+            None,
+            "nrSubmissionId",
+            Bundle ("fileType", 1),
+            LocalDate.now(),
+            Glacier(vaultName, archiveId)
+          )
+        )
+
+      "the retrieval service returns OK with results" in {
+        givenSearchReturns(searchQuery, OK, results)
+        search() shouldBe results
+      }
+
+      "the retrieval service returns ACCEPTED with results" in {
+        givenSearchReturns(searchQuery, ACCEPTED, results)
+        search() shouldBe results
+      }
+    }
+
+    "return an empty sequence" when {
+      val emptyResults = Seq.empty
+
+      "the retrieval service returns OK with empty results" in {
+        givenSearchReturns(searchQuery, OK, emptyResults)
+        search() shouldBe emptyResults
+      }
+
+      "the retrieval service returns ACCEPTED with empty results" in {
+        givenSearchReturns(searchQuery, ACCEPTED, emptyResults)
+        search() shouldBe emptyResults
+      }
+
+      "the retrieval service returns NOT_FOUND" in {
+        givenSearchReturns(searchQuery, NOT_FOUND)
+        search() shouldBe emptyResults
+      }
+    }
+
+    "fail" when {
+      "the retrieval service returns INTERNAL_SERVER_ERROR" in {
+        givenSearchReturns(searchQuery, INTERNAL_SERVER_ERROR)
+        anInternalServerErrorShouldBeThrownBy(search)
+      }
+
+      "the retrieval service returns BAD_GATEWAY" in {
+        givenSearchReturns(searchQuery, BAD_GATEWAY)
+        aBadGatewayErrorShouldBeThrownBy(search)
+      }
+    }
+  }
+
+  "getSubmissionBundle" should {
+    def submissionBundle(): WSResponse = connector.getSubmissionBundle(vaultName, archiveId, authorisedUser).futureValue
+
+    // the backend only returns OK here...
+    "return OK" when {
+      "the retrieval service returns OK" in {
+        givenGetSubmissionBundles(OK)
+        submissionBundle().status shouldBe OK
+      }
+    }
+  }
+
+  "submitRetrievalRequest" should {
+    def submitRetrievalRequest(): HttpResponse =
+      connector.submitRetrievalRequest(vaultName, archiveId, authorisedUser).futureValue
+
+    "return OK" when {
+      "the retrieval service returns OK" in {
+        givenPostSubmissionBundlesRetrievalRequestsReturns(OK)
+        submitRetrievalRequest().status shouldBe OK
+      }
+    }
+
+    "return ACCEPTED" when {
+      "the retrieval service returns ACCEPTED" in {
+        givenPostSubmissionBundlesRetrievalRequestsReturns(ACCEPTED)
+        submitRetrievalRequest().status shouldBe ACCEPTED
+      }
+    }
+
+    "fail" when {
+      "the retrieval service returns NOT_FOUND" in {
+        givenPostSubmissionBundlesRetrievalRequestsReturns(NOT_FOUND)
+        aNotFoundErrorShouldBeThrownBy(submitRetrievalRequest)
+      }
+
+      "the retrieval service returns INTERNAL_SERVER_ERROR" in {
+        givenPostSubmissionBundlesRetrievalRequestsReturns(INTERNAL_SERVER_ERROR)
+        anInternalServerErrorShouldBeThrownBy(submitRetrievalRequest)
+      }
+
+      "the retrieval service returns BAD_GATEWAY" in {
+        givenPostSubmissionBundlesRetrievalRequestsReturns(BAD_GATEWAY)
+        aBadGatewayErrorShouldBeThrownBy(submitRetrievalRequest)
+      }
+    }
+  }
+
   "statusSubmissionBundle" should {
-    def statusSubmissionBundle(): HttpResponse =
-      connector.statusSubmissionBundle("vaultName", "archiveId").futureValue
+    def statusSubmissionBundle(): HttpResponse = connector.statusSubmissionBundle(vaultName, archiveId).futureValue
 
     "return OK" when {
       "the retrieval service returns OK" in {
@@ -70,26 +206,17 @@ class NrsRetrievalContractSpec
     "fail" when {
       "the retrieval service returns NOT_FOUND" in {
         givenHeadSubmissionBundlesReturns(NOT_FOUND)
-
-        intercept[Exception] {
-          statusSubmissionBundle()
-        }.getCause.asInstanceOf[HttpException].responseCode shouldBe NOT_FOUND
+        aNotFoundErrorShouldBeThrownBy(statusSubmissionBundle)
       }
 
       "the retrieval service returns INTERNAL_SERVER_ERROR" in {
         givenHeadSubmissionBundlesReturns(INTERNAL_SERVER_ERROR)
-
-        intercept[Exception] {
-          statusSubmissionBundle()
-        }.getCause.asInstanceOf[UpstreamErrorResponse].statusCode shouldBe INTERNAL_SERVER_ERROR
+        anInternalServerErrorShouldBeThrownBy(statusSubmissionBundle)
       }
 
       "the retrieval service returns BAD_GATEWAY" in {
         givenHeadSubmissionBundlesReturns(BAD_GATEWAY)
-
-        intercept[Exception] {
-          statusSubmissionBundle()
-        }.getCause.asInstanceOf[UpstreamErrorResponse].statusCode shouldBe BAD_GATEWAY
+        aBadGatewayErrorShouldBeThrownBy(statusSubmissionBundle)
       }
     }
   }
--- it/uk/gov/hmrc/nrsretrievalfrontend/stubs/NrsRetrievalStubs.scala
+++ it/uk/gov/hmrc/nrsretrievalfrontend/stubs/NrsRetrievalStubs.scala
@@ -15,135 +15,33 @@
  */
 package uk.gov.hmrc.nrsretrievalfrontend.stubs
 
-import com.github.tomakehurst.wiremock.client.WireMock.{aResponse, stubFor, urlEqualTo, _}
+import com.github.tomakehurst.wiremock.client.WireMock._
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import models.{NrsSearchResult, SearchQuery}
+import play.api.libs.json.Json.toJson
 
 object NrsRetrievalStubs {
-//  def givenNrsSearchResultInit(callbackUrl: String): StubMapping =
-//    stubFor(
-//      post(urlEqualTo(s"/api/v2/init"))
-//        .withRequestBody(equalToJson(
-//          s"""
-//             |{[
-//             |  {
-//             |    "businessId": "VAT",
-//             |    "notableEvent": "vat-return",
-//             |    "payloadContentType": "application/json",
-//             |    "userSubmissionTimestamp": "2018-04-07T12:13:25.156Z",
-//             |    "identityData": {
-//             |      "internalId": "some-id",
-//             |      "externalId": "some-id",
-//             |      "agentCode": "TZRXXV",
-//             |      <snip>
-//             |    },
-//             |    "userAuthToken": "AbCdEf123456",
-//             |    "headerData": {
-//             |      "Gov-Client-Public-IP": "198.51.100.0",
-//             |      "Gov-Client-Public-Port": "12345",
-//             |      <snip>
-//             |    },
-//             |    "searchKeys": {
-//             |      "vrn": "123456789",
-//             |      "taxPeriodEndDate": "2018-04-05"
-//             |    },
-//             |    "nrSubmissionId": "beb6f5b8-f2c3-4d5a-8c72-0888fc1bbbfd",
-//             |    "payloadMessageDigest": "0ec61e556ae5087fec39b43cd45ee0dbd9e9b782a9b0a3ae6c9078406b42e6db",
-//             |    "signatureTimestamp": "2018-05-03T09:37:45Z",
-//             |    "submissionApiKeyDigest": "83ff85b1f17f13277e821b86c644f1a42f2ac024629ad0ee987170f760cb4c",
-//             |    "payloadMessageDigestType": "SHA-256",
-//             |    "bundle":{
-//             |      "fileType": "zip",
-//             |      "fileSize": "123456"
-//             |    },
-//             |    "glacier": {
-//             |      "vaultName": "vat-return-2018",
-//             |      "archiveId": "29381782738273"
-//             |    },
-//             |    "expiryDate": "2038-04-07"
-//             | },
-//             |
-//             |
-//             |  {
-//             |    "businessId": "VAT",
-//             |    "notableEvent": "VAT Return",
-//             |    "payloadContentType": "application/json",
-//             |    "userSubmissionTimestamp": "2018-04-07T12:13:25.156Z",
-//             |    "identityData": {
-//             |      "internalId": "some-id",
-//             |      "externalId": "some-id",
-//             |      "agentCode": "TZRXXV",
-//             |      <snip>
-//             |    },
-//             |    "userAuthToken": "AbCdEf123456",
-//             |    "headerData": {
-//             |      "Gov-Client-Public-IP": "198.51.100.0",
-//             |      "Gov-Client-Public-Port": "12345",
-//             |      <snip>
-//             |    },
-//             |    "searchKeys": {
-//             |      "vrn": "123456789",
-//             |      "taxPeriodEndDate": "2018-04-05"
-//             |    },
-//             |    "nrSubmissionId": "beb6f5b8-f2c3-4d5a-8c72-0888fc1bbbfd",
-//             |    "payloadMessageDigest": "0ec61e556ae5087fec39b43cd45ee0dbd9e9b782a9b0a3ae6c9078406b42e6db",
-//             |    "signatureTimestamp": "2018-05-03T09:37:45Z",
-//             |    "submissionApiKeyDigest": "83ff85b1f17f13277e821b86c644f1a42f2ac024629ad0ee987170f760cb4c",
-//             |    "payloadMessageDigestType": "SHA-256",
-//             |    "bundle":{
-//             |      "fileType": "zip",
-//             |      "fileSize": "123456"
-//             |    },
-//             |    "glacier": {
-//             |      "vaultName": "vat-return-2018",
-//             |      "archiveId": "29381782738273"
-//             |    },
-//             |    "expiryDate": "2038-04-07"
-//             | },
-//             |""".stripMargin)
-//        )
-//        .willReturn(
-//          aResponse()
-//            .withStatus(202)
-//            .withHeader(HeaderNames.???, callbackUrl)))
+  val vaultName = "vaultName"
+  val archiveId = "archiveId"
+
+  private val retrievalPath = "/nrs-retrieval"
+  private val submissionBundlesPath = s"$retrievalPath/submission-bundles/$vaultName/$archiveId"
+
+  def givenSearchReturns(searchQuery: SearchQuery, status: Int, results: Seq[NrsSearchResult]): StubMapping =
+    stubFor(get(urlEqualTo(s"$retrievalPath/submission-metadata?${searchQuery.searchText}"))
+      .willReturn(aResponse().withStatus(status).withBody(toJson(results).toString())))
+
+  def givenSearchReturns(searchQuery: SearchQuery, status: Int): StubMapping =
+    stubFor(get(urlEqualTo(s"$retrievalPath/submission-metadata?${searchQuery.searchText}"))
+      .willReturn(aResponse().withStatus(status)))
+
+  def givenGetSubmissionBundles(status: Int): StubMapping =
+    stubFor(get(urlEqualTo(submissionBundlesPath)).willReturn(aResponse().withStatus(status)))
+
+  def givenPostSubmissionBundlesRetrievalRequestsReturns(status: Int): StubMapping =
+    stubFor(post(urlEqualTo(s"$submissionBundlesPath/retrieval-requests"))
+      .willReturn(aResponse().withStatus(status)))
 
   def givenHeadSubmissionBundlesReturns(status: Int): StubMapping =
-    stubFor(
-      head(urlEqualTo(s"/nrs-retrieval/submission-bundles/vaultName/archiveId"))
-        .willReturn(aResponse().withStatus(status)))
-
-            //.withHeader(HeaderNames.???, s"/get-results/nrs-retrieval/submission-metadata?")))
-
-//  def givenNrsSearchResult(server: WireMockServer): StubMapping =
-//    stubFor(
-//      get(urlEqualTo(s"test/v1/api"))
-//        .willReturn(
-//          aResponse()
-//            .withStatus(404)
-//            .withHeader(HeaderNames.???, s"/get-results/nrs-retrieval/submission-metadata?")))
-
-//  def givenNrsSearchKeys(vrn: String, taxPeriodEndDate: String, userAuthToken: String
-//    stubFor(
-//      get(urlEqualTo(testUrl = s"/api/confirmed?id=$vrn"))
-//        .willReturn(
-//          aResponse()
-//            .withStatus(200)
-//            .withBody(s"""
-//              {
-//                 |    "businessId": "VAT" {
-//                 |    "notableEvent": "VAT Return" {
-//                 |    "payloadContentType": "application/json",
-//                 |    "userSubmissionTimestamp": "2018-04-07T12:13:25.156Z",
-//                 |    "identityData": {
-//                 |    "internalId": "some-id",
-//                 |     },
-//                 |    "searchKeys": [
-//                 |            "$vrn",
-//                 |            "$taxPeriodEndDate",
-//                 |        ],
-//                 |        "userAuthToken": "AbCdEf123456",
-//                 |    },
-//                 |    "nrSubmissionId": "beb6f5b8-f2c3-4d5a-8c72-0888fc1bbbfd",
-//                 |    "id": "GB990091234524"
-//                 |}
-//                 |""".stripMargin)))
+    stubFor(head(urlEqualTo(submissionBundlesPath)).willReturn(aResponse().withStatus(status)))
 }

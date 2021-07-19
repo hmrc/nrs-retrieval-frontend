4486ed34acb3d48efdf8accd03cb239c308e25ae
PaulHodgson <paul.hodgson073@gmail.com>
2021-07-19 16:47:23 +0100

    rationalise ViewConfig

--- app/config/ViewConfig.scala
+++ app/config/ViewConfig.scala
@@ -21,23 +21,7 @@ import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 import java.net.URL
 
-case class ViewConfig(
-                       appName:                       String,
-                       assetsPrefix:                  String,
-                       authUrl:                       String,
-                       loginUrl:                      String,
-                       signOut:                       String,
-                       frontendBaseUrl:               FrontendBaseUrlProvider,
-                       btaBaseUrl:                    String,
-                       viewAndChangeBaseUrl:          String,
-                       nddsBaseUrl:                   String,
-                       payFrontendBaseUrl:            String,
-                       govUkHomeUrl:                  String,
-                       accessibilityStatementBaseUrl: String,
-                       timeoutDialogTimeout:          Int,
-                       timeoutDialogCountdown:        Int
-                     ) {
-
+case class ViewConfig(appName: String, signOut: String, frontendBaseUrl: FrontendBaseUrlProvider) {
   /*
     * Everything what depends on the `frontendBaseUrl` has to be lazy. Otherwise it will be impossible to run tests
     * based on GuiceOneServerPerSuite because `port` at which the test app is served
@@ -45,44 +29,18 @@ case class ViewConfig(
     * is initialized during application start so you can not refer to it before application is started
     * and therefore override it in test configs.
     */
-  lazy val reportAProblemPartialUrl = s"${frontendBaseUrl.baseUrl()}/contact/problem_reports_ajax?service=$appName"
-  lazy val reportAProblemNonJSUrl = s"${frontendBaseUrl.baseUrl()}/contact/problem_reports_nonjs?service=$appName"
-  lazy val reportAccessibilityProblemUrl = s"${frontendBaseUrl.baseUrl()}/contact/accessibility?service=$appName"
   lazy val feedbackUrl = s"${frontendBaseUrl.baseUrl()}/contact/beta-feedback?service=$appName"
-  lazy val btaHomeUrl: String = s"$btaBaseUrl/business-account"
-  lazy val nddsUrl = s"$nddsBaseUrl/directdebits"
   lazy val host: String = new URL(frontendBaseUrl.baseUrl()).getHost
-  val govUkCdsSetupGuidancePageUrl: String = s"$govUkHomeUrl/guidance/set-up-a-direct-debit-for-a-duty-deferment-account-on-the-customs-declaration-service"
-
-  lazy val loginBaseUrl: String = frontendBaseUrl.baseUrl()
-
-  val chooseAWayToPayUrl = s"$payFrontendBaseUrl/pay/vat/choose-a-way-to-pay"
 
   @Inject
-  def this(
-            servicesConfig:  ServicesConfig,
-            frontendBaseUrl: FrontendBaseUrlProvider
-          ) = this(
-    appName                       = servicesConfig.getString("appName"),
-    assetsPrefix                  = servicesConfig.getString(s"assets.url") + servicesConfig.getString(s"assets.version"),
-    authUrl                       = servicesConfig.baseUrl("auth"),
-    loginUrl                      = servicesConfig.getString("urls.login"),
-    signOut                       = servicesConfig.getString("urls.logout"),
-    frontendBaseUrl               = frontendBaseUrl,
-    btaBaseUrl                    = servicesConfig.getString("btaBaseUrl"),
-    viewAndChangeBaseUrl          = servicesConfig.getString("viewAndChangeBaseUrl"),
-    nddsBaseUrl                   = servicesConfig.getString("nddsBaseUrl"),
-    payFrontendBaseUrl            = servicesConfig.getString("payFrontendBaseUrl"),
-    govUkHomeUrl                  = servicesConfig.getString("govUkHomeUrl"),
-    timeoutDialogTimeout          = servicesConfig.getInt("timeout-dialog.timeout"),
-    timeoutDialogCountdown        = servicesConfig.getInt("timeout-dialog.countdown"),
-    accessibilityStatementBaseUrl = servicesConfig.getString("accessibility-statement-frontend.url")
-  )
+  def this(servicesConfig:  ServicesConfig, frontendBaseUrl: FrontendBaseUrlProvider) =
+    this(
+      appName = servicesConfig.getString("appName"),
+      signOut = servicesConfig.getString("urls.logout"),
+      frontendBaseUrl = frontendBaseUrl)
 
   val cookiesUrl: String = "https://www.tax.service.gov.uk/help/cookies"
   val privacyNoticeUrl: String = "https://www.tax.service.gov.uk/help/privacy"
   val termsAndConditionsUrl: String = "https://www.tax.service.gov.uk/help/terms-and-conditions"
   val helpUsingGovUkUrl: String = "https://www.gov.uk/help"
-
-  def accessibilityStatementUrl(relativeUrl: String): String = s"$accessibilityStatementBaseUrl/accessibility-statement/direct-debit?referrerUrl=$payFrontendBaseUrl$relativeUrl"
 }

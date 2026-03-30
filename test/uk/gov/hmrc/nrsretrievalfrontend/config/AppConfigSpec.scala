8f450f363d473ca6a89a8a567b9da0defeb80424
AIbrahimQA <57941383+AIbrahimQA@users.noreply.github.com>
2026-03-30 15:43:06 +0100

    Fix test

--- test/uk/gov/hmrc/nrsretrievalfrontend/config/AppConfigSpec.scala
+++ test/uk/gov/hmrc/nrsretrievalfrontend/config/AppConfigSpec.scala
@@ -69,7 +69,8 @@ class AppConfigSpec extends BaseUnitSpec:
       appConfig.notableEvents.filter(_._2.metadataSearchKeys).keySet shouldBe Set(
         "itsa-ad-hoc-refund",
         "income-tax-view-change-adjust-payment-on-account",
-        "vap-application-received"
+        "vap-application-received",
+        "vap-withdraw-received"
       )
     }
   }
